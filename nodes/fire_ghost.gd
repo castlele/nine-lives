1ee03e9a63f9717419c1dc787c4bb5b3cfa76a2e
castlele <castlelecs@gmail.com>
2025-07-29 22:12:28 +0300

    feat(level_1): extract fire_ghost to a separate node

---
+++ nodes/fire_ghost.gd
@@ -0,0 +1,34 @@
+extends Sprite2D
+
+@onready var opacityTimer: Timer = $opacity_timer
+@onready var fireGhostOpacityLight: PointLight2D = $fire_ghost_light
+
+var incrementOpacity = true
+var isOpacityPaused = false
+
+func _ready() -> void:
+	incrementOpacity = not modulate.a >= 1
+
+
+func _process(delta: float) -> void:
+	if isOpacityPaused: return
+
+	if incrementOpacity:
+		modulate.a += delta
+		fireGhostOpacityLight.color.a += delta
+	else:
+		modulate.a -= delta
+		fireGhostOpacityLight.color.a -= delta
+
+	if modulate.a >= 1 and opacityTimer.is_stopped():
+		incrementOpacity = false
+		isOpacityPaused = true
+		opacityTimer.start()
+
+	if modulate.a <= 0 and opacityTimer.is_stopped():
+		incrementOpacity = true
+		isOpacityPaused = true
+		opacityTimer.start()
+
+func _on_opacity_timer_timeout() -> void:
+	isOpacityPaused = false
